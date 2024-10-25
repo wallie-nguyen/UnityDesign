126d17cd49d5fc1427b79f794ba41ccb3425960a
TuongNgo-Saritasa <tuong.ngo@saritasa.com>
2024-10-25 15:15:31 +0700

    Add Strategy

---
+++ DesignPattern/Assets/Strategy/SpellStrategy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpellStrategy : ScriptableObject
+{
+    public abstract void CastSpell(Transform origin);
+}
+
+public class ShieldSpell : SpellStrategy
+{
+    public GameObject shieldPrefab;
+
+    public override void CastSpell(Transform origin)
+    {
+        Instantiate(shieldPrefab, origin.position, origin.rotation);
+    }
+}
+
+public class Hero : MonoBehaviour
+{
+    public SpellStrategy spellStrategy;
+
+    public void CastSpell()
+    {
+        spellStrategy.CastSpell(transform);
+    }
+}
