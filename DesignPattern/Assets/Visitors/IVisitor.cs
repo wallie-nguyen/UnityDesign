b3b0dedf8df8fad8e73bb0911c0fad7f65222efb
TuongNgo-Saritasa <tuong.ngo@saritasa.com>
2024-11-01 17:53:46 +0700

    Add visitor

---
+++ DesignPattern/Assets/Visitors/IVisitor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVisitor
+{
+    void Visit<T>(T visitable) where T : Component, IVisitable;
+}
+
+public interface IVisitable
+{
+    void Accept(IVisitor visitor);
+}
